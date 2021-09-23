44785934ac7c4ceb19aa7166b1281280b2eb5f12
KHHaugaard <haugaard1101@gmail.com>
2021-09-23 11:29:40 +0200

    Lets go
    Dice start

---
+++ src/com/company/Dice.java
@@ -0,0 +1,31 @@
+package com.company;
+import java.util.Random
+public class Dice {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int MAX = 6;
+        int faceValue;
+        for() {
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
