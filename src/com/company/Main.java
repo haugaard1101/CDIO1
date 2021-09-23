8350c22ec26406ec8c2628152216c71967052b18
Abdu <Abdu0901@ltkskole.dk>
2021-09-23 11:59:11 +0200

    Added variables to main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        int player1Score = 0;
+        int player2Score = 0;
+
+        int diceOne = 0;
+        int diceTwo = 0;
+
+        int pointsToWin = 40;
+
+        boolean gameFinish = false;
 
 
     }
