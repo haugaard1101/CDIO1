bbabd889b63dc5b478025378e5f49cf1ee90f600
KHHaugaard <haugaard1101@gmail.com>
2021-09-23 12:08:34 +0200

    Dice

--- src/com/company/Dice.java
+++ src/com/company/Dice.java
@@ -3,18 +3,14 @@ import java.util.Random;
 public class Dice {
     public static void main(String[] args) {
         Random random = new Random();
-        int MAX = 6;
-        int Score;
-        int faceValue;
+        int faceValue1 = random.nextInt(6)+1;
+        int faceValue2 = random.nextInt(6)+1;
+        int sum = faceValue1 + faceValue2;
 
 
-
-
-
-
-
-
-
+        System.out.println(faceValue1);
+        System.out.println(faceValue2);
+        System.out.println(sum);
 
 
 
