6ef3dbf776f85163cd357233e8c8471569d3161e
KHHaugaard <haugaard1101@gmail.com>
2021-09-23 11:37:06 +0200

    Update Dice.java

--- src/com/company/Dice.java
+++ src/com/company/Dice.java
@@ -1,14 +1,15 @@
 package com.company;
-import java.util.Random
+import java.util.Random;
 public class Dice {
     public static void main(String[] args) {
         Random random = new Random();
         int MAX = 6;
+        int Score;
         int faceValue;
-        for() {
 
 
-        }
+
+
 
 
 
